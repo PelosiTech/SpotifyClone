1997f34b49bdcee90bc1545cdbf55db297cda520
Carlo Pelosi <cpelosi@asu.edu>
2021-01-17 21:36:34 -0700

    add a SongListItem component to work with

---
+++ components/SongListItem/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, Image, View } from 'react-native';
+
+import styles from './styles'
+import {Song} from "../../types";
+
+export type SongListItemProps = {
+    song: Song
+}
+
+const SongListItem = (props: SongListItemProps) => {
+    return <View>
+
+    </View>
+}
+
+export default SongListItem;
