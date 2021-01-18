1156adc7ec8e25b4297b6921cee9c98371be40b6
Carlo Pelosi <cpelosi@asu.edu>
2021-01-17 21:04:58 -0700

    add Navigation with TouchAbleWithoutFeedback on press sending the ID

--- components/Album/index.tsx
+++ components/Album/index.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { View, Image, Text} from "react-native";
+import { View, Image, Text, TouchableWithoutFeedback} from "react-native";
 import styles from "./styles";
 import {Album} from "../../types";
+import { useNavigation } from '@react-navigation/native'
 
 export type AlbumProps = {
     album: Album;
 }
 
 const AlbumComponent = (props: AlbumProps) => {
+    const navigation = useNavigation();
+
+    const onPress = () => {
+        navigation.navigate('AlbumScreen', {id: props.album.id});
+    }
+
     return (
-        <View style={styles.container}>
-            <Image source={{uri: props.album.imageUri}} style={styles.image}/>
-            <Text style={styles.text}>{props.album.artistsHeadline}</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={onPress}>
+            <View style={styles.container}>
+                    <Image source={{uri: props.album.imageUri}} style={styles.image}/>
+                    <Text style={styles.text}>{props.album.artistsHeadline}</Text>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
