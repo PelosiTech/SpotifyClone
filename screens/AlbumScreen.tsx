cdd3595983b6f558774e735bf45717c834bf00d4
Carlo Pelosi <cpelosi@asu.edu>
2021-01-17 21:27:24 -0700

    add some dummy data in to be able to show lists of these songs fromalbum

--- screens/AlbumScreen.tsx
+++ screens/AlbumScreen.tsx
@@ -1,7 +1,67 @@
-import  React from 'react';
+import  React, {useEffect} from 'react';
 import { View, Text } from 'react-native'
+import { useRoute } from '@react-navigation/native';
+
+const data = {
+    id: '11',
+    name: 'Good Vibes',
+    by: 'Spotify',
+    numberOfLikes: 38,
+    imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+    artistsHeadline: 'Taylor Swift, Kygo Objective C, Avicii',
+    songs: [
+        {
+        id: '1',
+        imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+        title: 'High on you',
+        artist: 'Carlo',
+        },
+        {
+            id: '2',
+            imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+            title: 'High on you',
+            artist: 'Carlo',
+        },
+        {
+            id: '3',
+            imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+            title: 'High on you',
+            artist: 'Carlo',
+        },
+        {
+            id: '4',
+            imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+            title: 'High on you',
+            artist: 'Carlo',
+        },
+        {
+            id: '5',
+            imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+            title: 'High on you',
+            artist: 'Carlo',
+        },
+        {
+            id: '6',
+            imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+            title: 'High on you',
+            artist: 'Carlo',
+        },
+        {
+            id: '7',
+            imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
+            title: 'High on you',
+            artist: 'Carlo',
+        }
+    ]
+}
 
 const AlbumScreen = () => {
+    const route = useRoute();
+
+    useEffect(() => {
+        console.log(route)
+    }, [])
+
     return <View>
         <Text style={{color: 'white'}}>
             Album Screen
