4fbc821413f2ba65ab0930a6e82ea0db53a17aeb
Carlo Pelosi <cpelosi@asu.edu>
2021-01-17 21:04:42 -0700

    add AlbumScreen basic .tsx to show

---
+++ screens/AlbumScreen.tsx
@@ -0,0 +1,12 @@
+import  React from 'react';
+import { View, Text } from 'react-native'
+
+const AlbumScreen = () => {
+    return <View>
+        <Text style={{color: 'white'}}>
+            Album Screen
+        </Text>
+    </View>
+}
+
+export default AlbumScreen;
