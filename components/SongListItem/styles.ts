e5ffdc95e137f1c3ce596ae84e3329b05e556a1b
Carlo Pelosi <cpelosi@asu.edu>
2021-01-17 21:48:20 -0700

    add some styles so the component shows up with image

---
+++ components/SongListItem/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    title: {
+        color: "white",
+    },
+    artist: {
+        color: "lightgray"
+    },
+    image: {
+        width: 75,
+        height: 75
+    }
+});
+
+export default styles;
