8148f50615f3c86bfb450b0ef722db22e6dddae4
Carlo Pelosi <cpelosi@asu.edu>
2021-01-19 12:44:25 -0700

    add play/pause feature to stop and play song also changing the icon

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,10 +1,11 @@
-import React, {useEffect} from 'react';
-import { Text, Image, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {Text, Image, View, TouchableOpacity} from 'react-native';
 import { Audio } from "expo-av";
 
 import styles from './styles'
 import {Song} from "../../types";
 import {AntDesign, FontAwesome} from "@expo/vector-icons";
+import {Sound} from "expo-av/build/Audio/Sound";
 
 const song = {
     id: '1',
@@ -15,23 +16,42 @@ const song = {
 }
 
 const PlayerWidget = () => {
+    const [sound, setSound] = useState<Sound|null>(null);
+    const [isPlaying, setIsPlaying] = useState(true);
 
-    const onPlaybackStatusUpdate = (status) => {
+    const onPlaybackStatusUpdate = (status: any) => {
         console.log(status)
+        setIsPlaying(status.isPlaying)
     }
 
     const playCurrentSong = async () => {
-        const { sound } = Audio.Sound.createAsync(
+        if(sound) {
+            await sound.unloadAsync();
+        }
+
+        const { sound: newSound } = await Audio.Sound.createAsync(
             { uri: song.uri },
-            { shouldPlay: true},
+            { shouldPlay: isPlaying},
             onPlaybackStatusUpdate
 
         );
+        setSound(newSound);
     }
 
     useEffect(() => {
         playCurrentSong();
     },[])
+
+    const onPlayPausePress = async () => {
+        if(!sound) {
+            return
+        }
+        if(isPlaying) {
+            await sound.stopAsync();
+        } else {
+            await sound.playAsync();
+        }
+    }
 
 
     return (
@@ -44,7 +64,9 @@ const PlayerWidget = () => {
             </View>
             <View style={styles.iconContainer}>
                 <AntDesign name="hearto" size={30} color={"white"} />
-                <FontAwesome name="play" size={30} color={"white"} />
+                <TouchableOpacity onPress={onPlayPausePress}>
+                    <FontAwesome name={isPlaying ? 'pause' : 'play'} size={30} color={"white"} />
+                </TouchableOpacity>
             </View>
         </View>
     </View>
