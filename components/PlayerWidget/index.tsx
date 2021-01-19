58509626ff8412e4fd26aec71de815e765d2fce1
Carlo Pelosi <cpelosi@asu.edu>
2021-01-19 11:32:10 -0700

    use Audio from expo-av to playSong with useEffect on start up

--- components/PlayerWidget/index.tsx
+++ components/PlayerWidget/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Text, Image, View } from 'react-native';
+import { Audio } from "expo-av";
 
 import styles from './styles'
 import {Song} from "../../types";
@@ -7,6 +8,7 @@ import {AntDesign, FontAwesome} from "@expo/vector-icons";
 
 const song = {
     id: '1',
+    uri: 'https://not-just-trash.s3-eu-west-1.amazonaws.com/WhatsApp+Audio+2020-09-22+at+14.20.25.mp4',
     imageUri: 'https://cache.boston.com/resize/bonzai-fba/Globe_Photo/2011/04/14/1302796985_4480/539w.jpg',
     title: 'High on you',
     artist: 'Carlo',
@@ -14,7 +16,26 @@ const song = {
 
 const PlayerWidget = () => {
 
-    return <View style={styles.container}>
+    const onPlaybackStatusUpdate = (status) => {
+        console.log(status)
+    }
+
+    const playCurrentSong = async () => {
+        const { sound } = Audio.Sound.createAsync(
+            { uri: song.uri },
+            { shouldPlay: true},
+            onPlaybackStatusUpdate
+
+        );
+    }
+
+    useEffect(() => {
+        playCurrentSong();
+    },[])
+
+
+    return (
+    <View style={styles.container}>
         <Image source={{ uri: song.imageUri }} style={styles.image} />
         <View style={styles.rightContainer}>
             <View style={styles.nameContainer}>
@@ -27,6 +48,7 @@ const PlayerWidget = () => {
             </View>
         </View>
     </View>
+    )
 }
 
 export default PlayerWidget;
