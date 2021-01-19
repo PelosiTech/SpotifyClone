04feabc02570bf0a50ce69f67133cc38dc04a9d5
Carlo Pelosi <cpelosi@asu.edu>
2021-01-18 19:27:58 -0700

    add styles to our Playerwidget

---
+++ components/PlayerWidget/styles.ts
@@ -0,0 +1,47 @@
+import { StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute",
+        bottom: 49,
+        width: '100%',
+        backgroundColor: "#131313",
+        flexDirection: 'row',
+        borderColor: 'black',
+        borderWidth: 2,
+        alignItems: 'center',
+    },
+    rightContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: "space-between",
+    },
+    nameContainer: {
+        flexDirection: "row",
+        alignItems: 'center',
+    },
+    title: {
+        color: "white",
+        fontSize: 18,
+        fontWeight: 'bold',
+        margin: 10,
+    },
+    artist: {
+        color: "lightgray",
+        fontSize: 18,
+    },
+    iconContainer: {
+        flexDirection: "row",
+        alignItems: 'center',
+        width: 100,
+        justifyContent: 'space-around',
+    },
+    image: {
+        width: 75,
+        height: 75,
+        marginRight: 10,
+    },
+
+});
+
+export default styles;
