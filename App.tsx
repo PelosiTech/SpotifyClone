9e64274e86402fb673a45f5fa8b275837eaf0226
Carlo Pelosi <cpelosi@asu.edu>
2021-01-20 17:53:56 -0700

    add AppContext to hold globally the state of what song is playing

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -10,18 +10,26 @@ import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
+import { AppContext } from "./AppContext";
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [songId, setSongId] = useState<String | null>(null);
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-        <PlayerWidget />
+          <AppContext.Provider value={{
+              songId,
+              setSongId: (id: string) => setSongId(id)
+          }}>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+            <PlayerWidget />
+          </AppContext.Provider>
       </SafeAreaProvider>
     );
   }
